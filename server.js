bd36c9e999ef716495ecda10654caf01eb64f0ed
avisre <90497762+avisre@users.noreply.github.com>
2024-10-30 14:05:01 +0000

    Update server.js

--- server.js
+++ server.js
@@ -408,6 +408,6 @@ app.get('/video', (req, res) => {
 });
 
 // Start the server and listen on the specified port (PORT)
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
 });
